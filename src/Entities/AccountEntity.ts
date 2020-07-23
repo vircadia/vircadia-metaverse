22ab7c19a0efdffedc9302ff2a56dfcf361296b7
Robert Adams <misterblue@misterblue.com>
2020-07-23 15:14:16 -0700

    Change copyright to "Vircadia Contributors"

--- src/Entities/AccountEntity.ts
+++ src/Entities/AccountEntity.ts
@@ -1,4 +1,4 @@
-//   Copyright 2020 Robert Adams
+//   Copyright 2020 Vircadia Contributors
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
