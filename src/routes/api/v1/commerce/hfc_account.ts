660a1fd9333e75ee23b0a7f9755ca6de17807c70
Robert Adams <misterblue@misterblue.com>
2020-08-10 19:28:09 -0700

    Requests that don't do much but are generated by Interface scripts: Implement /api/v1/commerce/available_updates.ts Implement /api/v1/commerce/hfc_account.ts Implement /api/v1/commerce/history.ts Implement /api/v1/user_stories.ts

---
+++ src/routes/api/v1/commerce/hfc_account.ts
@@ -0,0 +1,42 @@
+//   Copyright 2020 Vircadia Contributors
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+'use strict';
+
+import { Router, RequestHandler, Request, Response, NextFunction } from 'express';
+import { setupMetaverseAPI, finishMetaverseAPI } from '@Route-Tools/middleware';
+import { accountFromAuthToken } from '@Route-Tools/middleware';
+
+import { Logger } from '@Tools/Logging';
+
+// PUT /api/v1/commerce/htc_account
+const procPutCommerceHfcAccount: RequestHandler = async (req: Request, resp: Response, next: NextFunction) => {
+  if (req.vAuthAccount) {
+    Logger.debug('procPutCommerceHfcAccount');
+  }
+  else {
+    req.vRestResp.respondFailure('unauthorized');
+  };
+  next();
+};
+
+export const name = '/api/v1/commerce/hfc_account';
+
+export const router = Router();
+
+router.put('/api/v1/commerce/hfc_account', [ setupMetaverseAPI,
+                                             accountFromAuthToken,
+                                             procPutCommerceHfcAccount,
+                                             finishMetaverseAPI ] );
+
---
+++ src/routes/api/v1/user_stories.ts
@@ -0,0 +1,36 @@
+//   Copyright 2020 Vircadia Contributors
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+'use strict';
+
+import { Router, RequestHandler, Request, Response, NextFunction } from 'express';
+import { setupMetaverseAPI, finishMetaverseAPI } from '@Route-Tools/middleware';
+
+// Sample request from Interface scripts
+// GET /api/v1/user_stories?include_actions=announcement&restriction=open,hifi&require_online=true&protocol=Qa12Qv2cYYxmNV8c%2Fqlf5A%3D%3D&per_page=10 4
+const procGetUserStories: RequestHandler = (req: Request, resp: Response, next: NextFunction) => {
+  if (req.vRestResp) {
+    req.vRestResp.Data = {
+    };
+  }
+  next();
+};
+
+export const name = '/api/v1/user_stories';
+
+export const router = Router();
+
+router.get('/api/v1/user_stories',  [ setupMetaverseAPI,
+                                      procGetUserStories,
+                                      finishMetaverseAPI ] );
