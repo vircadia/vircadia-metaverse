74b5aac017e645f2252f33c6137cc20e0ef6e8c1
Robert Adams <misterblue@misterblue.com>
2020-09-27 06:41:05 -0700

    Adding some notes on future changes in Entity.ts

--- src/Entities/Entity.ts
+++ src/Entities/Entity.ts
@@ -14,6 +14,37 @@
 
 'use strict';
 
-export class Entity {
+import { createObject, getObject, getObjects, updateObjectFields, deleteOne, noCaseCollation } from '@Tools/Db';
+import { CriteriaFilter } from '@Entities/EntityFilters/CriteriaFilter';
+import { GenUUID, genRandomString, IsNullOrEmpty, IsNotNullOrEmpty } from '@Tools/Misc';
+
+export abstract class Entity {
+  /*
+  // This is experimental development.
+  // All entities should share functions so they can be handled as general items.
+  // But since the objects returned by the database are just Objects and not
+  //     created as instances of their classes, they don't have initialized class-ness.
+  public id: string;
+  public collection: string;
+
+  async getWithId(pId: string): Promise<Entity> {
+    return IsNullOrEmpty(pId) ? null : getObject(this.collection, { 'id': pId });
+  };
+  async create(pId: string): Promise<Entity> {
+    return undefined;
+  };
+  async add(pEntity: Entity): Promise<Entity> {
+    return createObject(this.collection, pEntity);
+  };
+  async remove(pEntity: Entity): Promise<boolean> {
+    return deleteOne(this.collection, { 'id': pEntity.id } );
+  };
+  async *enumerateAsync(pPager: CriteriaFilter,
+               pInfoer: CriteriaFilter, pScoper: CriteriaFilter): AsyncGenerator<Entity> {
+    for await (const acct of getObjects(this.collection, pPager, pInfoer, pScoper)) {
+      yield acct;
+    };
+  };
+  */
 };
 
