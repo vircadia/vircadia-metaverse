4137f56673a9ba7d88ec59635e0447885a6ff2c4
Kalila L <somnilibertas@gmail.com>
2020-12-22 00:41:41 -0500

    Add copyright headers + fix whitespace on createVersion.js

--- build_scripts/cleanDist.js
+++ build_scripts/cleanDist.js
@@ -1,3 +1,13 @@
+/*
+    cleanDist.js
+
+    Created by Kalila L. on Dec 20 2020.
+    Copyright 2020 Vircadia contributors.
+
+    Distributed under the Apache License, Version 2.0.
+    See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
 const fs = require('fs').promises;
 
 const directory = './dist';
