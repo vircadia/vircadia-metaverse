608e8a2f33cbc7eb12b694608ff58152e4a5d643
Robert Adams <misterblue@misterblue.com>
2021-04-03 07:44:39 -0700

    Update tabbing to 4 spaces in token.ts. No functional change.

--- src/routes/oauth/token.ts
+++ src/routes/oauth/token.ts
@@ -38,121 +38,122 @@ import { Logger } from '@Tools/Logging';
 //    is trying to mimic a standard /oauth/token request. So this
 //    code builds a response and finishes the request.
 const procPostOauthToken: RequestHandler = async (req: Request, resp: Response, next: NextFunction) => {
-  let respBody: VKeyedCollection;
-  try {
-    const accessGrantType = req.body.grant_type;
-    switch (accessGrantType) {
-      case 'password': {
-        // There are several types of "password"s passed by Interface:
-        // PLAIN PASSWORD
-        const userName = req.body.username;
-        const userPassword = req.body.password;
+    let respBody: VKeyedCollection;
+    try {
+        const accessGrantType = req.body.grant_type;
+        switch (accessGrantType) {
+            case 'password': {
+                // There are several types of "password"s passed by Interface:
+                // PLAIN PASSWORD
+                const userName = req.body.username;
+                const userPassword = req.body.password;
 
-        // STEAM
-        // string userPassword = req.body.steam_auth_ticket;
+                // STEAM
+                // string userPassword = req.body.steam_auth_ticket;
 
-        // OCULUS
-        // string userPassword = req.body.oculus_nonce;
-        // string userPassword = req.body.oculus_id;
+                // OCULUS
+                // string userPassword = req.body.oculus_nonce;
+                // string userPassword = req.body.oculus_id;
 
-        const userScope: string = req.body.scope ?? TokenScope.OWNER;
-        if (TokenScope.KnownScope(userScope)) {
-          let aAccount = await Accounts.getAccountWithUsername(userName);
-          if (IsNullOrEmpty(aAccount)) {
-            // people can log in with either their username or their email
-            aAccount = await Accounts.getAccountWithEmail(userName);
-          };
-          if (aAccount) {
-            if (await Accounts.validatePassword(aAccount, userPassword)) {
-              Logger.debug(`procPostOAuthToken: login of user ${userName}`);
-              const tokenInfo = await Tokens.createToken(aAccount.id, [ userScope ]);
-              await Tokens.addToken(tokenInfo);
-              respBody = buildOAuthResponseBody(aAccount, tokenInfo);
-            }
-            else {
-              respBody = buildOAuthErrorBody('Invalid password');
-              req.vRestResp.IsFailure = true;
+                const userScope: string = req.body.scope ?? TokenScope.OWNER;
+                if (TokenScope.KnownScope(userScope)) {
+                    let aAccount = await Accounts.getAccountWithUsername(userName);
+                    if (IsNullOrEmpty(aAccount)) {
+                        // people can log in with either their username or their email
+                        aAccount = await Accounts.getAccountWithEmail(userName);
+                    };
+                    if (aAccount) {
+
+                        if (await Accounts.validatePassword(aAccount, userPassword)) {
+                            Logger.debug(`procPostOAuthToken: login of user ${userName}`);
+                            const tokenInfo = await Tokens.createToken(aAccount.id, [ userScope ]);
+                            await Tokens.addToken(tokenInfo);
+                            respBody = buildOAuthResponseBody(aAccount, tokenInfo);
+                        }
+                        else {
+                            respBody = buildOAuthErrorBody('Invalid password');
+                            req.vRestResp.IsFailure = true;
+                        };
+                    }
+                    else{
+                        respBody = buildOAuthErrorBody('Unknown user');
+                        req.vRestResp.IsFailure = true;
+                    };
+                }
+                else {
+                    respBody = buildOAuthErrorBody('Invalid scope');
+                    req.vRestResp.IsFailure = true;
+                };
+                break;
             };
-          }
-          else{
-            respBody = buildOAuthErrorBody('Unknown user');
-            req.vRestResp.IsFailure = true;
-          };
-        }
-        else {
-          respBody = buildOAuthErrorBody('Invalid scope');
-          req.vRestResp.IsFailure = true;
+            case 'authorization_code': {
+                respBody = buildOAuthErrorBody('Do not know what to do with an authorization_code');
+                req.vRestResp.IsFailure = true;
+                break;
+            };
+            case 'refresh_token': {
+                const refreshingToken = req.body.refresh_token;
+                const refreshToken = await Tokens.getTokenWithRefreshToken(refreshingToken);
+                if (Tokens.hasNotExpired(refreshToken)) {
+                    const requestingAccount = await Accounts.getAccountWithAuthToken(req.vRestResp.getAuthToken());
+                    if (requestingAccount && refreshToken.accountId === requestingAccount.id) {
+                        // refresh token has not expired and requestor is owner of the token so make new
+                        const newToken = await Tokens.createToken(req.vAuthAccount.id, refreshToken.scope);
+                        await Tokens.addToken(newToken);
+                        respBody = buildOAuthResponseBody(requestingAccount, newToken);
+                    }
+                    else {
+                        respBody = buildOAuthErrorBody('refresh token not owned by accessing account');
+                        req.vRestResp.IsFailure = true;
+                    };
+                }
+                else {
+                    respBody = buildOAuthErrorBody('refresh token expired');
+                    req.vRestResp.IsFailure = true;
+                }
+                break;
+            };
+            default: {
+                respBody = buildOAuthErrorBody('Unknown grant_type: ' + accessGrantType);
+                req.vRestResp.IsFailure = true;
+                break;
+            };
         };
-        break;
-      };
-      case 'authorization_code': {
-        respBody = buildOAuthErrorBody('Do not know what to do with an authorization_code');
+    }
+    catch (err) {
+        respBody = buildOAuthErrorBody('Exception: ' + err);
         req.vRestResp.IsFailure = true;
-        break;
-      };
-      case 'refresh_token': {
-        const refreshingToken = req.body.refresh_token;
-        const refreshToken = await Tokens.getTokenWithRefreshToken(refreshingToken);
-        if (Tokens.hasNotExpired(refreshToken)) {
-          const requestingAccount = await Accounts.getAccountWithAuthToken(req.vRestResp.getAuthToken());
-          if (requestingAccount && refreshToken.accountId === requestingAccount.id) {
-            // refresh token has not expired and requestor is owner of the token so make new
-            const newToken = await Tokens.createToken(req.vAuthAccount.id, refreshToken.scope);
-            await Tokens.addToken(newToken);
-            respBody = buildOAuthResponseBody(requestingAccount, newToken);
-          }
-          else {
-            respBody = buildOAuthErrorBody('refresh token not owned by accessing account');
-            req.vRestResp.IsFailure = true;
-          };
-        }
-        else {
-          respBody = buildOAuthErrorBody('refresh token expired');
-          req.vRestResp.IsFailure = true;
-        }
-        break;
-      };
-      default: {
-        respBody = buildOAuthErrorBody('Unknown grant_type: ' + accessGrantType);
-        req.vRestResp.IsFailure = true;
-        break;
-      };
     };
-  }
-  catch (err) {
-    respBody = buildOAuthErrorBody('Exception: ' + err);
-    req.vRestResp.IsFailure = true;
-  };
 
-  req.vRestResp.Data = respBody;
-  next();
+    req.vRestResp.Data = respBody;
+    next();
 };
 
 // The response to /oauth/token is special since it tries to mimic an official interface.
 // This is also used by /api/v1/token/new to return the created token.
 export function buildOAuthResponseBody(pAcct: AccountEntity, pToken: AuthToken): VKeyedCollection {
-  const body: VKeyedCollection = {
-    'access_token': pToken.token,
-    'token_type': 'Bearer',
-    'expires_in': pToken.expirationTime.valueOf()/1000 - pToken.whenCreated.valueOf()/1000,
-    'refresh_token': pToken.refreshToken,
-    'scope': pToken.scope[0],
-    'created_at': pToken.whenCreated.valueOf() / 1000,
-  };
-  if (pAcct) {
-    body.account_id = pAcct.id,
-    body.account_name = pAcct.username,
-    body.account_roles = pAcct.roles;
-  };
-  return body;
+    const body: VKeyedCollection = {
+        'access_token': pToken.token,
+        'token_type': 'Bearer',
+        'expires_in': pToken.expirationTime.valueOf()/1000 - pToken.whenCreated.valueOf()/1000,
+        'refresh_token': pToken.refreshToken,
+        'scope': pToken.scope[0],
+        'created_at': pToken.whenCreated.valueOf() / 1000,
+    };
+    if (pAcct) {
+        body.account_id = pAcct.id,
+        body.account_name = pAcct.username,
+        body.account_roles = pAcct.roles;
+    };
+    return body;
 };
 
 
 function buildOAuthErrorBody(pMsg: string): VKeyedCollection {
-  Logger.error(`procPostOauthToken: returning error body: ${pMsg}`);
-  return {
-    'error': pMsg
-  };
+    Logger.error(`procPostOauthToken: returning error body: ${pMsg}`);
+    return {
+        'error': pMsg
+    };
 };
 
 export const name = "/oauth/token";
