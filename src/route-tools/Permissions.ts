edb3c26041b014d1750297a843c64262704a3a98
Robert Adams <misterblue@misterblue.com>
2021-06-13 13:13:29 -0700

    Non-functional fixing of source indentation in route-tools/Permissions.ts.

--- src/route-tools/Permissions.ts
+++ src/route-tools/Permissions.ts
@@ -44,108 +44,110 @@ export async function checkAccessToEntity(pAuthToken: AuthToken,  // token being
                             pRequiredAccess: string[],          // permissions required to access domain
                             pRequestingAccount?: AccountEntity  // requesting account if known
                     ): Promise<boolean> {
-  let requestingAccount = pRequestingAccount;
-  let canAccess: boolean = false;
-  if (IsNotNullOrEmpty(pTargetEntity)) {
-    for (const perm of pRequiredAccess) {
-      Logger.cdebug('field-setting', `checkAccessToEntity: checking ${perm}`);
-      switch (perm) {
-        case Perm.ALL:
-          canAccess = true;
-          break;
-        case Perm.PUBLIC:
-          // The target entity is publicly visible
-          // Mostly AccountEntities that must have an 'availability' field
-          if (pTargetEntity.hasOwnProperty('availability')) {
-            if ((pTargetEntity as AccountEntity).availability.includes(Availability.ALL)) {
-              canAccess = true;
+
+    let requestingAccount = pRequestingAccount;
+    let canAccess: boolean = false;
+
+    if (IsNotNullOrEmpty(pTargetEntity)) {
+        for (const perm of pRequiredAccess) {
+            Logger.cdebug('field-setting', `checkAccessToEntity: checking ${perm}`);
+            switch (perm) {
+                case Perm.ALL:
+                    canAccess = true;
+                    break;
+                case Perm.PUBLIC:
+                    // The target entity is publicly visible
+                    // Mostly AccountEntities that must have an 'availability' field
+                    if (pTargetEntity.hasOwnProperty('availability')) {
+                        if ((pTargetEntity as AccountEntity).availability.includes(Availability.ALL)) {
+                            canAccess = true;
+                        };
+                    };
+                    break;
+                case Perm.DOMAIN:
+                    // requestor is a domain and it's account is the domain's sponsoring account
+                    if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.DOMAIN)) {
+                        if (pTargetEntity.hasOwnProperty('sponsorAccountId')) {
+                            Logger.cdebug('field-setting', `checkAccessToEntity: authToken is domain. auth.AccountId=${pAuthToken.accountId}, sponsor=${(pTargetEntity as any).sponsorAccountId}`);
+                            canAccess = pAuthToken.accountId === (pTargetEntity as any).sponsorAccountId;
+                        }
+                        else {
+                            // Super special case where domain doesn't have a sponsor but has an api_key.
+                            // In this case, the API_KEY is put in the accountId field of the DOMAIN scoped AuthToken
+                            if (pTargetEntity.hasOwnProperty('apiKey')) {
+                                canAccess = pAuthToken.accountId === (pTargetEntity as any).apiKey;
+                            };
+                        };
+                    };
+                    break;
+                case Perm.OWNER:
+                    // The requestor wants to be the same account as the target entity
+                    if (pAuthToken && pTargetEntity.hasOwnProperty('id')) {
+                        canAccess = pAuthToken.accountId === (pTargetEntity as AccountEntity).id;
+                    };
+                    if (!canAccess && pTargetEntity.hasOwnProperty('accountId')) {
+                        canAccess = pAuthToken.accountId === (pTargetEntity as any).accountId;
+                    };
+                    break;
+                case Perm.FRIEND:
+                    // The requestor is a 'friend' of the target entity
+                    if (pAuthToken && pTargetEntity.hasOwnProperty('friends')) {
+                        const targetFriends: string[] = (pTargetEntity as AccountEntity).friends;
+                        if (targetFriends) {
+                            requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
+                            canAccess = SArray.hasNoCase(targetFriends, requestingAccount.username);
+                        };
+                    };
+                    break;
+                case Perm.CONNECTION:
+                    // The requestor is a 'connection' of the target entity
+                    if (pAuthToken && pTargetEntity.hasOwnProperty('connections')) {
+                        const targetConnections: string[] = (pTargetEntity as AccountEntity).connections;
+                        if (targetConnections) {
+                            requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
+                            canAccess = SArray.hasNoCase(targetConnections, requestingAccount.username);
+                        };
+                    };
+                    break;
+                case Perm.ADMIN:
+                    if (pAuthToken && Tokens.isSpecialAdminToken(pAuthToken)) {
+                        Logger.cdebug('field-setting', `checkAccessToEntity: isSpecialAdminToken`);
+                        canAccess = true;
+                    }
+                    else {
+                        // If the authToken is an account, has access if admin
+                        if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.OWNER)) {
+                            Logger.cdebug('field-setting', `checkAccessToEntity: admin. auth.AccountId=${pAuthToken.accountId}`);
+                            requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
+                            canAccess = Accounts.isAdmin(requestingAccount);
+                        };
+                    };
+                    break;
+                case Perm.SPONSOR:
+                    // Requestor is a regular account and is the sponsor of the domain
+                    if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.OWNER)) {
+                        if (pTargetEntity.hasOwnProperty('sponsorAccountId')) {
+                            Logger.cdebug('field-setting', `checkAccessToEntity: authToken is domain. auth.AccountId=${pAuthToken.accountId}, sponsor=${(pTargetEntity as any).sponsorAccountId}`);
+                            canAccess = pAuthToken.accountId === (pTargetEntity as DomainEntity).sponsorAccountId;
+                        };
+                    };
+                    break;
+                case Perm.DOMAINACCESS:
+                    // Target entity has a domain reference and verify the requestor is able to reference that domain
+                    if (pAuthToken && pTargetEntity.hasOwnProperty('domainId')) {
+                        const aDomain = await Domains.getDomainWithId((pTargetEntity as any).domainId);
+                        if (aDomain) {
+                            canAccess = aDomain.sponsorAccountId === pAuthToken.accountId;
+                        };
+                    };
+                    break;
+                default:
+                    canAccess = false;
+                    break;
             };
-          };
-          break;
-        case Perm.DOMAIN:
-          // requestor is a domain and it's account is the domain's sponsoring account
-          if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.DOMAIN)) {
-            if (pTargetEntity.hasOwnProperty('sponsorAccountId')) {
-              Logger.cdebug('field-setting', `checkAccessToEntity: authToken is domain. auth.AccountId=${pAuthToken.accountId}, sponsor=${(pTargetEntity as any).sponsorAccountId}`);
-              canAccess = pAuthToken.accountId === (pTargetEntity as any).sponsorAccountId;
-            }
-            else {
-              // Super special case where domain doesn't have a sponsor but has an api_key.
-              // In this case, the API_KEY is put in the accountId field of the DOMAIN scoped AuthToken
-              if (pTargetEntity.hasOwnProperty('apiKey')) {
-                canAccess = pAuthToken.accountId === (pTargetEntity as any).apiKey;
-              };
-            };
-          };
-          break;
-        case Perm.OWNER:
-          // The requestor wants to be the same account as the target entity
-          if (pAuthToken && pTargetEntity.hasOwnProperty('id')) {
-            canAccess = pAuthToken.accountId === (pTargetEntity as AccountEntity).id;
-          };
-          if (!canAccess && pTargetEntity.hasOwnProperty('accountId')) {
-            canAccess = pAuthToken.accountId === (pTargetEntity as any).accountId;
-          };
-          break;
-        case Perm.FRIEND:
-          // The requestor is a 'friend' of the target entity
-          if (pAuthToken && pTargetEntity.hasOwnProperty('friends')) {
-            const targetFriends: string[] = (pTargetEntity as AccountEntity).friends;
-            if (targetFriends) {
-              requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
-              canAccess = SArray.hasNoCase(targetFriends, requestingAccount.username);
-            };
-          };
-          break;
-        case Perm.CONNECTION:
-          // The requestor is a 'connection' of the target entity
-          if (pAuthToken && pTargetEntity.hasOwnProperty('connections')) {
-            const targetConnections: string[] = (pTargetEntity as AccountEntity).connections;
-            if (targetConnections) {
-              requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
-              canAccess = SArray.hasNoCase(targetConnections, requestingAccount.username);
-            };
-          };
-          break;
-        case Perm.ADMIN:
-          if (pAuthToken && Tokens.isSpecialAdminToken(pAuthToken)) {
-            Logger.cdebug('field-setting', `checkAccessToEntity: isSpecialAdminToken`);
-            canAccess = true;
-          }
-          else {
-            // If the authToken is an account, has access if admin
-            if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.OWNER)) {
-              Logger.cdebug('field-setting', `checkAccessToEntity: admin. auth.AccountId=${pAuthToken.accountId}`);
-              requestingAccount = requestingAccount ?? await Accounts.getAccountWithId(pAuthToken.accountId);
-              canAccess = Accounts.isAdmin(requestingAccount);
-            }
-          };
-          break;
-        case Perm.SPONSOR:
-          // Requestor is a regular account and is the sponsor of the domain
-          if (pAuthToken && SArray.has(pAuthToken.scope, TokenScope.OWNER)) {
-            if (pTargetEntity.hasOwnProperty('sponsorAccountId')) {
-              Logger.cdebug('field-setting', `checkAccessToEntity: authToken is domain. auth.AccountId=${pAuthToken.accountId}, sponsor=${(pTargetEntity as any).sponsorAccountId}`);
-              canAccess = pAuthToken.accountId === (pTargetEntity as DomainEntity).sponsorAccountId;
-            };
-          };
-          break;
-        case Perm.DOMAINACCESS:
-          // Target entity has a domain reference and verify the requestor is able to reference that domain
-          if (pAuthToken && pTargetEntity.hasOwnProperty('domainId')) {
-            const aDomain = await Domains.getDomainWithId((pTargetEntity as any).domainId);
-            if (aDomain) {
-              canAccess = aDomain.sponsorAccountId === pAuthToken.accountId;
-            };
-          };
-          break;
-        default:
-          canAccess = false;
-          break;
-      }
-      // If some permission allows access, we are done
-      if (canAccess) break;
+            // If some permission allows access, we are done
+            if (canAccess) break;
+        };
     };
-  };
-  return canAccess;
+    return canAccess;
 };
