6e4981cfa8986663178020d4d2d20f1cf671c886
Daniel Lyne <dlyne@pm.me>
2021-12-31 22:32:12 -0500

    Make stylistic changes to tests

--- tests/Entities/Accounts.unit.test.ts
+++ tests/Entities/Accounts.unit.test.ts
@@ -1,15 +1,14 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient } from "mongodb";
 
 import { AccountEntity } from "../../src/Entities/AccountEntity";
 import { Accounts } from "../../src/Entities/Accounts";
-import { Datab, setDB } from "../../src/Tools/Db";
-import { Config, initializeConfiguration} from "../../src/config";
+import { setDB } from "../../src/Tools/Db";
 
 async function testDatab() {
-    let connectUrl = process.env.CONNECT_URL;
-    let dbName = process.env.DB_NAME;
+    const connectUrl = process.env.CONNECT_URL;
+    const dbName = process.env.DB_NAME;
 
-    let baseClient = await MongoClient.connect(connectUrl, {
+    const baseClient = await MongoClient.connect(connectUrl, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     });
@@ -52,48 +51,46 @@ function mockAccount(user: string): AccountEntity {
         roles: null,
         IPAddrOfCreator: null,
         whenCreated: null,
-        timeOfLastHeartbeat: null,
+        timeOfLastHeartbeat: null
 
-    }
+    };
 }
 
 describe("Entities/Accounts - unit tests", () => {
     test("getAccountWithId returns null with invalid input", () => {
         testDatab()
-        .then(() => {
-            initializeConfiguration();
-            return Accounts.getAccountWithId("").then(resp => {
-                expect(resp).toBeNull();
+            .then(() => {
+                return Accounts.getAccountWithId("").then((resp) => {
+                    expect(resp).toBeNull();
+                });
             });
-        });
     });
 
     test("getAccountWithId returns null with no inserted accounts", () => {
         testDatab()
-        .then(() => {
-            initializeConfiguration();
-            return Accounts.getAccountWithId("id").then(resp => {
-                expect(resp).toBeNull();
+            .then(() => {
+                return Accounts.getAccountWithId("id").then((resp) => {
+                    expect(resp).toBeNull();
+                });
             });
-        });
     });
 
     test("addAccount, then geting it by ID, then removing it", () => {
         testDatab()
-        .then(() => {
-            let acc_expected = mockAccount("test3");
-            Accounts.addAccount(acc_expected)
-            .then((acc) => {
-                Accounts.getAccountWithId(acc.id)
-                .then((fetched) => {
-                    expect(fetched.username).toBe(acc_expected.username);
-                    expect(fetched.email).toBe(acc_expected.email);
+            .then(() => {
+                const accExpected = mockAccount("test3");
+                Accounts.addAccount(accExpected)
+                    .then((acc) => {
+                        Accounts.getAccountWithId(acc.id)
+                            .then((fetched) => {
+                                expect(fetched.username).toBe(accExpected.username);
+                                expect(fetched.email).toBe(accExpected.email);
 
-                    return Accounts.removeAccount(fetched).then(resp => {
-                        expect(resp).toBe(true);
+                                return Accounts.removeAccount(fetched).then((resp) => {
+                                    expect(resp).toBe(true);
+                                });
+                            });
                     });
-                })
             });
-        });
     });
 });
